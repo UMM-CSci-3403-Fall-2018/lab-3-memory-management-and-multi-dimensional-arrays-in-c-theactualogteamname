d25b7399118f6f483b0f706b1866274c0315b352
Mark Schauer <marksthe1@gmail.com>
2018-10-01 22:23:59 -0500

    Add `array_merge.c` and write code beginnings

---
+++ array_merge/array_merge.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "array_merge.h"
+#include "../mergesort/mergesort.c"
+
+int* array_merge(int num_arrays,int* sizes, int** values){
+	int* result;
+	int fSize = 0;
+
+	return result;
+
+}
+
