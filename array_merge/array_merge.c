9d9d8b7a4cf960fbb95ee6b88a7f901661ce146b
Mark Schauer <schau303@neoprene.morris.umn.edu>
2018-10-02 10:07:50 -0500

    Add some for loops in `array_merge.c`

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -4,8 +4,27 @@
 #include "../mergesort/mergesort.c"
 
 int* array_merge(int num_arrays,int* sizes, int** values){
-	int* result;
+	
 	int fSize = 0;
+	int index = 0;
+	int* temp = (int*) calloc(fSize, sizeof(int));
+	int* result;
+
+	for (int i = 0; i < num_arrays; i++){
+		fSize += sizes[i];
+	}
+
+	for (int n = 0; n < sizes[j]; n++){
+		temp[index] = values[j][n];
+		index++
+	}
+}
+
+	mergesort(fSize, temp);
+	
+
+
+
 
 	return result;
 
